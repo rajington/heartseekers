c8aeaa12c7d3e1cfd56a7becc5682e818ff7e025
Rajarshi Nigam <gitlab@raj.oib.com>
2016-04-26 18:11:17 -0400

    add region default

--- summoner/handler.js
+++ summoner/handler.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import { getSummoner } from './lolapi';
 
-export default async function ({ region, name }) {
+export default async function ({ region = 'na', name }) {
   const summoner = await getSummoner(region, name);
   return summoner;
 }
