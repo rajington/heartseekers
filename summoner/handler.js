ef0c5c3d9e5ebb2ef6779bb4af745e42d48520b3
Rajarshi Nigam <gitlab@raj.oib.com>
2016-04-26 18:44:18 -0400

    removed default region

--- summoner/handler.js
+++ summoner/handler.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import { getSummoner, getChampions } from './lolapi';
 
-export default async function ({ region = 'na', name }) {
+export default async function ({ region, name }) {
   const summoner = await getSummoner(region, name);
   const champions = await getChampions(region, summoner.id);
   return {
