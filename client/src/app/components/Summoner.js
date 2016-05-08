e73e34e3c9bf55583bab277eb30f51df1625c104
Rajarshi Nigam <gitlab@raj.oib.com>
2016-05-08 15:33:14 -0400

    display additional summoner information

--- client/src/app/components/Summoner.js
+++ client/src/app/components/Summoner.js
@@ -6,8 +6,20 @@ import PromiseStateContainer from './PromiseStateContainer';
 const Summoner = ({ summonerFetch }) => (
   <PromiseStateContainer
     ps={summonerFetch}
-    onFulfillment={({ summoner }) => (
-      <p>{summoner.name}</p>
+    onFulfillment={({ summoner, score, champions }) => (
+      <div>
+        <p>{summoner.name}</p>
+        <p>{summoner.level}</p>
+        <img src={`http://ddragon.leagueoflegends.com/cdn/6.9.1/img/profileicon/${summoner.icon}.png`} alt="icon" />
+        <p>{score}</p>
+        <ul>
+        {
+          champions.map((champion, index) => (
+            <li key={index}>{champion.highestGrade}</li>
+          ))
+        }
+        </ul>
+      </div>
     )}
   />
 );
