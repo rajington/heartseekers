4cf67df785397de763fba85240f7fb83b4899082
Rajarshi Nigam <gitlab@raj.oib.com>
2016-05-08 14:41:29 -0400

    made the router's hash history queryless

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { render } from 'react-dom';
-import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { Router, IndexRoute, Route } from 'react-router';
 import App from './components/App';
 import Main from './components/Main';
 import Summoner from './components/Summoner';
 import NoMatch from './components/NoMatch';
+import history from './history';
 
 // Needed for onTouchTap
 // Can go away when react 1.0 release
@@ -13,8 +14,9 @@ import NoMatch from './components/NoMatch';
 // https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
+// useRouterHistory creates a composable higher-order function
 render((
-  <Router history={browserHistory}>
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Main} />
       <Route path="/summoners/:region/:name" component={Summoner} />
